fde4a3de725543920de43b336a46ac29ce7998c5
MishaRocks <tehobox@yandex.ru>
2023-07-23 01:05:45 +0300

    first 19_2 + *

--- catalog/views.py
+++ catalog/views.py
@@ -8,4 +8,9 @@ def index(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
     return render(request, 'catalog/contacts.html')
