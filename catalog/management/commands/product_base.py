6668449691dd83053325a85e048c1562fad54116
MishaRocks <tehobox@yandex.ru>
2023-07-26 13:02:39 +0300

    hw 20.1 tasks 1-6 + custom function + category custom

--- catalog/management/commands/product_base.py
+++ catalog/management/commands/product_base.py
@@ -1,8 +1,9 @@
 import json
+import psycopg2
 
 from django.core.management import BaseCommand
 
-from catalog.models import Product
+from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
@@ -11,7 +12,17 @@ class Command(BaseCommand):
         Product.objects.all().delete()
 
         with open('product_data.json', 'r', encoding='utf8') as file:
-            data = json.load(file)
+            product_data = json.load(file)
 
-        for p in data:
-            Product.objects.create(**p['fields'])
+        for p in product_data:
+            Product.objects.create(**p)
+
+        Category.objects.all().delete()
+
+        category_data = [
+            {'name': "Техника", 'description': "Разная техника: компьютеры, телефоны, планшеты"},
+            {'name': "Аксессуары", 'description': "Чехлы для телефонов, зарядные кабеля и устройства, блоки питания"},
+            {'name': "Товары для дома", 'description': "Тут просто будет всё остальное, что не техника"}
+            ]
+        for c in category_data:
+            Product.objects.create(c['fields'])
