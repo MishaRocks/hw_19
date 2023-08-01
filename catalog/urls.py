affef341dc26a337b47cb64f31c8cc7afa8cf2d7
MishaRocks <tehobox@yandex.ru>
2023-08-01 10:12:32 +0300

    hw 21.1 tasks 1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, product
+from catalog.views import contacts, ProductListView, ProductDetailView, BlogpostCreateView, BlogpostListView, \
+    BlogpostDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', ProductListView.as_view(), name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', product, name='product')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('new_post/', BlogpostCreateView.as_view(), name='new_post'),
+    path('blog/', BlogpostListView.as_view(), name='blog'),
+    path('blog/<int:pk>/', BlogpostDetailView.as_view(), name='post'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from catalog.models import Product, Blogpost
 
-# Create your views here.
-from catalog.models import Product
 
-
-def index(request):
-    context = {
-        'object_list': Product.objects.all(),
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/index.html'
+    extra_context = {
         'title': 'Новые товары',
         'about': 'Последние поступления товаров представлены ниже. Странно сформулировано, да?'
     }
-    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
@@ -27,10 +27,52 @@ def contacts(request):
     return render(request, 'catalog/contacts.html', context)
 
 
-def product(request, pk):
-    show_product = Product.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.get(pk=pk),
-        'title': f'В продаже: {show_product.name}',
-    }
-    return render(request, 'catalog/product.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(pk=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        show_product = Product.objects.get(pk=self.kwargs.get('pk'))
+        context_data['object_list'] = Product.objects.get(pk=self.kwargs.get('pk')),
+        context_data['title'] = f'В продаже: {show_product.name}'
+
+        return context_data
+
+class BlogpostCreateView(CreateView):
+    model = Blogpost
+    fields = ('title', 'content', 'preview', 'is_published')
+    success_url = reverse_lazy('catalog:index')
+
+
+class BlogpostListView(ListView):
+    model = Blogpost
+    template_name = 'blogpost_list.html'
+
+
+class BlogpostDetailView(DetailView):
+    model = Blogpost
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.showed += 1
+        self.object.save()
+        return self.object
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(pk=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        show_product = Blogpost.objects.get(pk=self.kwargs.get('pk'))
+        context_data['object_list'] = Blogpost.objects.get(pk=self.kwargs.get('pk')),
+
+        return context_data
